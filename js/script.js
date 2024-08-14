13217bbc8a6dc71c73acdc786befed5f04e3cc20
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-08-14 12:26:38 -0700

    Typing Effect JS function added!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Add custom JavaScript here
+// Function #1: Typing Effect //
+function runTypingEffect() {
+    // |-----| Variables |-----| //
+    const text = 'I am Abril Ramirez.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 100;
+    // |-----| Variables |-----| //
+
+    // |-----| Function call |-----| //
+    typeText(text, typingElement, typingDelay);
+    // |-----| Function call |-----| //
+}
+
+// Function #2: Typing Delay //
+function typeText(text, typingElement, delay) {
+    for(let i = 0; i < text.length; i++) {
+        setTimeout(() => { 
+            typingElement.textContent += text.charAt(i);
+        }, delay * i);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect());
